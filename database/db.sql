769ee0b61a0a3c56c58f26771f809e28adbd7f4a
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2024-04-28 16:19:32 +0200

    Added more strict rules to database schema, added created_at column in measurement table

--- database/db.sql
+++ database/db.sql
@@ -1,30 +1,32 @@
 CREATE TABLE person (
   id SERIAL PRIMARY KEY,
-  name VARCHAR,
-  password VARCHAR
+  username VARCHAR UNIQUE NOT NULL,
+  password VARCHAR NOT NULL
 );
 
 CREATE TABLE active_session (
   id SERIAL PRIMARY KEY,
-  person_id INTEGER,
-  token VARCHAR,
+  person_id INTEGER NOT NULL,
+  token VARCHAR UNIQUE NOT NULL,
   FOREIGN KEY (person_id) REFERENCES person (id)
 );
 
 CREATE TABLE measurement (
   id SERIAL PRIMARY KEY,
-  topic_id INTEGER,
-  value FLOAT,
+  topic_id INTEGER NOT NULL,
+  value FLOAT NOT NULL,
+  created_at TIMESTAMP NOT NULL,
   FOREIGN KEY (topic_id) REFERENCES topic (id)
 );
 
 CREATE TABLE topic (
   id SERIAL PRIMARY KEY,
-  country VARCHAR,
-  city VARCHAR,
-  building VARCHAR,
-  space VARCHAR,
-  sensor VARCHAR
+  country VARCHAR NOT NULL,
+  city VARCHAR NOT NULL,
+  building VARCHAR NOT NULL,
+  space VARCHAR NOT NULL,
+  sensor VARCHAR NOT NULL,
+  UNIQUE(country, city, building, space, sensor)
 );
 
 
