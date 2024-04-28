5235eeae1b84b09f5266848869e2c80bddb8dcdb
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2024-04-28 16:47:10 +0200

    Added cors, added support for json in request and now logging endpoint

--- auth-api/src/server.js
+++ auth-api/src/server.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 require('dotenv').config()
-// Routes
 
+app.use(express.json())
+app.use(logEndpoint)
+app.use(cors())
+
+// Routes
 const loginRoute = require('./routes/login')
 // const tokenRoute = require('./routes/token')
 const logoutRoute = require('./routes/logout')
 app.use("/login", loginRoute)
 // app.use("/tokens", tokenRoute)
 app.use("/logout", logoutRoute)
+
+function logEndpoint(req, res, next) {
+    console.log(req.originalUrl)
+    console.log(req.body)
+    next()
+}
 
 app.listen(3000)
