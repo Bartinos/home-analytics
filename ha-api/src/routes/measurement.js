8d9d7b9a1ca16a699d0f3b204db3c7158e0f15ab
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2024-05-01 16:42:45 +0200

    Added authentication to ha-api

--- ha-api/src/routes/measurement.js
+++ ha-api/src/routes/measurement.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const measurementController = require('../controllers/measurementController')
 const { getMeasurementsValidation, postMeasurementValidation } = require('../validations/measurementValidation')
+const { authenticate } = require('../middleware/authentication')
 
-router.get("/", getMeasurementsValidation, measurementController.getMeasurements)
-router.post("/", postMeasurementValidation, measurementController.postMeasurement)
+router.get("/", authenticate, getMeasurementsValidation, measurementController.getMeasurements)
+router.post("/", authenticate, postMeasurementValidation, measurementController.postMeasurement)
 
 module.exports = router
--- ha-api/src/routes/topic.js
+++ ha-api/src/routes/topic.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const topicController = require('../controllers/topicController')
+const { authenticate } = require('../middleware/authentication')
 
-router.get("/", topicController.getAllTopics)
+router.get("/", authenticate, topicController.getAllTopics)
 
 module.exports = router
