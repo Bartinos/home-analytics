3b2f1ff2604814d837376759fe71baa24736a2fb
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2024-05-09 15:51:33 +0200

    Implemented parseAv1.ts

---
+++ data-saver/src/parsers/parseAv1.ts
@@ -0,0 +1,18 @@
+import Joi from 'joi';
+const MAX_SUPPORTED_UNIX_DATE = 8640000000000 // 2243-10-17T00:00:00.000Z, prevent new Date() from creating invalid date
+const MIN_SUPPORTED_UNIX_DATE = -8640000000000
+
+const av1PacketFormatScheme = Joi.object().keys({
+  type: Joi.string().required(),
+  value: Joi.required(),
+  timestamp: Joi.date().timestamp('unix').min(MIN_SUPPORTED_UNIX_DATE).max(MAX_SUPPORTED_UNIX_DATE)
+});
+
+function parseAv1(topic, message) {
+  const isValidAv1Format = av1PacketFormatScheme.validate(message);
+  return isValidAv1Format;
+}
+
+exports = {
+  parseAv1
+}
--- data-saver/src/saver.ts
+++ data-saver/src/saver.ts
@@ -1,23 +1,20 @@
-import fs from 'fs';
-
-fs;
-//
-console.log("test");
-
 import mqtt from "mqtt"; // import namespace "mqtt"
+const MQTT_AV1_TOPIC = "netherlands/breda/home/+/+";
+const MQTT_OLD_TOPIC = "home/livingroom/#";
 let client = mqtt.connect("mqtt://localhost:1883"); // create a client
 
 
 client.on("connect", () => {
-  client.subscribe("presence", (err) => {
+  client.subscribe(MQTT_OLD_TOPIC, (err) => {
     if (!err) {
-      client.publish("presence", "Hello mqtt");
     }
   });
+
 });
 
 client.on("message", (topic, message) => {
   // message is Buffer
+  console.log(topic);
   console.log(message.toString());
-  client.end();
+  // client.end();
 });
