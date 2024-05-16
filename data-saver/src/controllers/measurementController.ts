f104fb0676466343baa70d8798629bc835b5174a
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2024-05-16 13:04:26 +0200

    Implemented measurementRequestBody.ts

--- data-saver/src/controllers/measurementController.ts
+++ data-saver/src/controllers/measurementController.ts
@@ -1,10 +1,11 @@
 import { Av1Packet } from "../models/av1Packet.js";
 import { Av1Topic } from "../models/av1Topic.js";
+import { MeasurementRequestBody } from "../models/measurementRequestBody.js";
 import { parseAv1Packet } from "../parsers/parseAv1.js";
 
 const sendMeasurement = async (measurementTopic: Av1Topic, message: string) => {
   const av1Packet: Av1Packet = parseAv1Packet(message);
-  const av1MeasurementRequestBody = undefined;
+  const av1MeasurementRequestBody: MeasurementRequestBody = MeasurementRequestBody.fromAv1PacketAndAv1Topic(av1Packet, measurementTopic);
 }
 
 export {
--- data-saver/src/models/measurementRequestBody.ts
+++ data-saver/src/models/measurementRequestBody.ts
@@ -2,6 +2,14 @@ import { Av1Packet } from "./av1Packet.js";
 import { Av1Topic } from "./av1Topic.js";
 
 export class MeasurementRequestBody {
+  readonly country: string;
+  readonly city: string;
+  readonly building: string;
+  readonly space: string;
+  readonly sensor: string;
+  readonly value: any;
+  readonly timestamp: number;
+
   constructor(
     country: string,
     city: string,
@@ -11,10 +19,25 @@ export class MeasurementRequestBody {
     value: any,
     timestamp?: number
   ) {
-
+    this.country = country;
+    this.city = city;
+    this.building = building;
+    this.space = space;
+    this.sensor = sensor;
+    this.value = value;
+    if(timestamp) this.timestamp = timestamp;
   }
   static fromAv1PacketAndAv1Topic(av1Packet: Av1Packet, av1Topic: Av1Topic): MeasurementRequestBody {
-    const measurementRequestBody = new MeasurementRequestBody();
-
+    const timestamp: number = av1Packet.timestamp? av1Packet.timestamp : null;
+    const measurementRequestBody = new MeasurementRequestBody(
+      av1Topic.country,
+      av1Topic.city,
+      av1Topic.building,
+      av1Topic.space,
+      av1Topic.sensor,
+      av1Packet.value,
+      timestamp
+    );
+    return measurementRequestBody;
   }
 }
