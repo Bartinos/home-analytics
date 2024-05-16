b18eb92d3a26046a5b703976d66e53afde774e3b
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2024-05-16 13:44:41 +0200

    measurementService can now send measurements

--- data-saver/src/controllers/measurementController.ts
+++ data-saver/src/controllers/measurementController.ts
@@ -2,10 +2,12 @@ import { Av1Packet } from "../models/av1Packet.js";
 import { Av1Topic } from "../models/av1Topic.js";
 import { MeasurementRequestBody, measurementRequestBodyFromAv1PacketAndAv1Topic } from "../models/measurementRequestBody.js";
 import { parseAv1Packet } from "../parsers/parseAv1.js";
+import { postMeasurement } from "../services/measurementService.js";
 
 const sendMeasurement = async (measurementTopic: Av1Topic, message: string) => {
   const av1Packet: Av1Packet = parseAv1Packet(message);
   const av1MeasurementRequestBody: MeasurementRequestBody = measurementRequestBodyFromAv1PacketAndAv1Topic(av1Packet, measurementTopic);
+  await postMeasurement(av1MeasurementRequestBody);
 }
 
 export {
--- data-saver/src/services/measurementService.ts
+++ data-saver/src/services/measurementService.ts
@@ -2,10 +2,22 @@ import { Av1Packet } from "../models/av1Packet.js";
 import { Av1Topic } from "../models/av1Topic.js";
 import { MeasurementRequestBody } from "../models/measurementRequestBody.js";
 
-const headers = {
+const HEADERS = {
   'Content-type': 'application/json; charset=UTF-8',
-}
+};
+const API_URL = "localhost:3001";
 
-async function postMeasurment(measurementRequestBody: MeasurementRequestBody) {
-    
+export async function postMeasurement(measurementRequestBody: MeasurementRequestBody) {
+  const response = await fetch(
+    API_URL + "/measurements",
+    {
+      method: 'POST', 
+      body: JSON.stringify(measurementRequestBody),
+      headers: HEADERS
+    }
+  );
+
+  const data = await response.json();
+  console.log(data);
+
 }
