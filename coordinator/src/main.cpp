8d8f0465bc48b8f96255d404160426868ee6d28c
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2024-05-16 10:50:43 +0200

    Removed commented code in main

--- coordinator/src/main.cpp
+++ coordinator/src/main.cpp
@@ -9,28 +9,6 @@
 #include "zigbee/XbeeSerialHandler.h"
 #include "zigbeeToMqtt/XbeeReadingToMqttController.h"
 
-// // Initialize nodes
-//
-// byte ENDN2_MAC[8] = {0x00, 0x13, 0xA2, 0x00, 0x40, 0x8B, 0x17, 0xF4};
-// PotentiometerSensor potentiometerSensor;
-// XbeeNode endn2 = XbeeNode("ENDN2", "heater", ENDN2_MAC, &potentiometerSensor);
-// byte ENDN3_MAC[8] = {0x00, 0x13, 0xA2, 0x00, 0x41, 0x93, 0x0B, 0x47};
-// LdrSensor ldrSensor;
-// XbeeNode endn3 = XbeeNode("ENDN3", "livingroom", ENDN3_MAC, &ldrSensor);
-// // byte ENDN4_MAC[8] = {0x00, 0x13, 0xA2, 0x00, 0x40, 0x69, 0x50, 0xF3};
-// // XbeeNode endn4 = XbeeNode("ENDN4", ENDN4_MAC, new TemperatureSensor());
-//
-// byte ROUTER1_MAC[8] = {0x00, 0x13, 0xA2, 0x00, 0x41, 0x93, 0x0b, 0x09};
-// TemperatureSensor temperatureSensor;
-// XbeeNode router1 = XbeeNode("ROUTER1", "stairs", ROUTER1_MAC, &temperatureSensor);
-//
-// std::vector<XbeeNode> xbeeNodes ={
-//   endn2,
-//   endn3,
-//   router1
-// };
-
-// Consider using shared pointer
 EthernetClient ethernetClient;
 EthernetController ethernetController(&ethernetClient);
 
@@ -61,5 +39,4 @@ void loop() {
     XbeeReading xbeeReading = xbeeSerialHandler.createReadingFromSerial();
     xbeeReadingToMqttController.xbeeReadingToMqtt(xbeeReading);
   }
-  // Serial.print("running");
 }
