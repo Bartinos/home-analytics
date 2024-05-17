d2be28afdba89047f47fddc404c7203367e7ab2a
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2024-05-17 10:39:33 +0200

    Users actions are now an actionGroup

--- frontend/src/app/state/user/user.actions.ts
+++ frontend/src/app/state/user/user.actions.ts
@@ -1,11 +1,23 @@
-import { createAction, emptyProps, props} from '@ngrx/store';
-import { LoginRequest } from '../../models/loginRequest.interface';
-export const loginUser = createAction(
-  '[User] Login',
-  props<{request: LoginRequest}>()
-);
+import { createAction, createActionGroup, emptyProps, props} from '@ngrx/store';
+import { LoginRequest } from '../../shared/models/loginRequest.interface';
+import { CurrentUser } from '../../shared/models/currentUser.interface';
+// import { }
 
-export const loginUserSuccess = createAction(
-  '[User] Succesfully logged in',
-  // emptyProps()
-)
+export const userActions = createActionGroup({
+  source: 'user',
+  events: {
+    Login: props<{request: LoginRequest}>(),
+    'Login success': props<{currentUser: CurrentUser}>(),
+    'Login failure': emptyProps()
+  }
+})
+
+// export const loginUser = createAction(
+//   '[User] Login',
+//   props<{request: LoginRequest}>()
+// );
+//
+// export const loginUserSuccess = createAction(
+//   '[User] Succesfully logged in',
+//   // emptyProps()
+// )
