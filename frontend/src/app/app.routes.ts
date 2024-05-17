9ed81c2cb7e0cee9ce1edd267399e342ed0c48d3
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2024-05-17 13:30:03 +0200

    User is now redirected to login if not logged in, or to dashboard if logged in

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 import { isLoggedInGuard } from './shared/guards/is-logged-in.guard';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path: '', canActivateChild: [isLoggedInGuard], children: [
-    // { path: '', redirectTo: '/', pathMatch: 'full'}
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    { path: 'login', redirectTo: 'dashboard'},
+    { path: 'dashboard', component: DashboardComponent}
   ]},
 { path: 'login', component: AuthComponent }
 ];
